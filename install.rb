31bdfad42ec83e82f75aa364f53d0869cad85f49
jMeixner <jjm3333@gmail.com>
2015-12-22 00:14:10 -0600

    add renameing and directory changing run with ruby vim/install.rb

--- install.rb
+++ install.rb
@@ -1,6 +1,11 @@
+system('mv vim/ .vim/')
+system('cd .vim')
 puts "git start:"
 system('git submodule init')
 system('git submodule update')
+puts "Pulgin Install"c
+system('vim +PluginInstall +qa')
+
+system('cd ..')
 puts "all done!"
 
-system('vim +PluginInstall +qa')
