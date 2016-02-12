ba158b8b1f69790d125a9eba63ece2bf5ed123c2
tsral <tsral@jdocker21.tsrappdev.com>
2016-02-12 08:55:54 -0600

    imporve the script for installing .vim

--- install.sh
+++ install.sh
@@ -7,4 +7,14 @@ then
         mv .vimrc vimrc.old
 fi
 mv vim .vim
-ruby .vim/install.rb
+cd .vim
+echo "git start"
+git submodule init
+git submodule update
+echo "plugin install"
+vim +PluginInstall +qa
+
+cd ../
+echo "all done!"
+
+#ruby .vim/install.rb
